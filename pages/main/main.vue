1a50474307a10fe7d8a53a257ccdffad2bf84eaa
lwenm <1322806298@qq.com>
2019-11-26 23:06:31 +0800

    添加详情页面

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -5,7 +5,15 @@
 
 			</cu-custom>
 		</view>
-
+			<view class="cu-bar search bg-white">
+				<view class="search-form round">
+					<text class="cuIcon-search"></text>
+					<input @focus="InputFocus" @blur="InputBlur" :adjust-position="false" type="text" placeholder="搜索图片、文章、视频" confirm-type="search"></input>
+				</view>
+				<view class="action">
+					<button class="cu-btn bg-green shadow-blur round">搜索</button>
+				</view>
+			</view>
 		<swiper class="card-swiper" :class="dotStyle ? 'square-dot' : 'round-dot'" :indicator-dots="true" :circular="true"
 		 :autoplay="true" interval="5000" duration="500" @change="cardSwiper" indicator-color="#8799a3"
 		 indicator-active-color="#0081ff">
